18bbef5477426e029f86c87db548a6469631437b
temp25-30 <329814051+temp25-30@users.noreply.github.com>
2026-09-20 06:46:31 +0800

    Add files via upload

---
+++ OLFUVMV.java
@@ -0,0 +1,15 @@
+class Main {
+    public static void main(String args[]) {
+        System.out.println("VISION: ");
+        System.out.println("A premier inclusive university of choice aspiring to improve man as man by developing individuals through a legacy of excellent education and compassionate value formation.\n");
+
+        System.out.println("MISSION: ");
+        System.out.println("The Our Lady of Fatima University, together with Fatima Medical Science Foundation, Inc., is dedicated to the improvement of man as man through the holistic formation of individuals imbued with knowledge, skills, and virtues.\n");
+
+        System.out.println("CORE VALUES: ");
+        System.out.println("R - Respect\n");
+        System.out.println("I -  Integrity\n");
+        System.out.println("S - Service\n");
+        System.out.println("E - Excellence");
+    }
+}
